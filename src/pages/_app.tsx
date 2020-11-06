ff5f958e7bf2cce7aaae06c56f2bf332a7d7e248
Nelson Faiçal Rechdan <rechdanfr@gmail.com>
2020-11-06 17:02:52 -0300

    Added initial Scss file. Added App title. Update TypeScript config file.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
 // next
 import { AppProps } from 'next/app';
+import NextHead from 'next/head';
 
 // npm
 import React from 'react';
+
+// css
+import 'assets/scss/index.scss';
 
 // app
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<NextHead>
+				<title>WYD Private Tools</title>
+			</NextHead>
+
 			<Component {...pageProps} />
 		</>
 	);
